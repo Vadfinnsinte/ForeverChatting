0340b3d026cb1e67f7c8ee3d5e98bb7e637e2462
Linda Westerlund <lindawesterlund94@gmail.com>
2024-10-16 15:05:02 +0200

    added a list of rooms

---
+++ backend-src/data/rooms.ts
@@ -0,0 +1,25 @@
+import { Room } from "../Interfaces/Room"
+
+const rooms: Room[] = [
+
+    {
+       name: "Chit-Chat Castle",
+       status: true
+   },
+   
+   {
+       name: "Meme Mansion",
+       status: false
+   },
+   
+    {
+       name: "Giggle Galaxy",
+       status: true
+   },
+   
+   {
+       name: "Banter Basement",
+       status: false
+   },
+
+]
